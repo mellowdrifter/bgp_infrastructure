fc67bfc871389ed210af0ef2eb40f0846979aa46
Darren O'Connor <mellow.drifter@gmail.com>
2019-05-09 21:42:34 -0400

    tweeter get prefix now forumlates tweet update

--- tweeter/tweeter.py
+++ tweeter/tweeter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import bgpinfo_pb2 as pb
 import bgpinfo_pb2_grpc
 import configparser
@@ -14,6 +15,11 @@ config.read(path)
 server = str(config.get('grpc', 'server'))
 port = str(config.get('grpc', 'port'))
 log = config.get('grpc', 'logfile')
+
+# Check arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-test", help="test mode will read, but not tweet", default=True)
+args = parser.parse_args()
 
 # Set up logging
 format_string = '%(levelname)s: %(asctime)s: %(message)s'
@@ -38,7 +44,33 @@ def getCurrent():
      - ASNs? Not sure anymore
     """
     result = stub.get_prefix_count(pb.empty())
-    print(result)
+    
+    # Calculate deltas
+    ipv4_deltaH = result.active_4 - result.sixhoursv4
+    ipv6_deltaH = result.active_6 - result.sixhoursv6
+    ipv4_deltaW = result.active_4 - result.weekagov4
+    ipv6_deltaW = result.active_6 - result.weekagov6
+
+    # Calculate large subnet percentages
+    percent_v4 = (round(result.slash24 / float(result.active_4)*100, 2))
+    percent_v6 = (round(result.slash48 / float(result.active_6)*100, 2))
+
+    # Formulate update
+    delta4 = "I see " + str(result.active_4) + " IPv4 prefixes" + ". "
+    delta6 = "I see " + str(result.active_6) + " IPv6 prefixes" + ". "
+    delta4 += create_message(ipv4_deltaH, ipv4_deltaW)
+    delta6 += create_message(ipv6_deltaH, ipv6_deltaW)
+    delta4 += '. ' + str(percent_v4) + '% of prefixes are /24.'
+    delta6 += '. ' + str(percent_v6) + '% of prefixes are /48.'
+
+    if args.test:
+      print(delta4)
+      print(delta6)
+    else:
+      tweet(4, delta4, None)
+      tweet(6, delta6, None)
+      setTweetBit(result.time)
+
 
 def getWeek():
     """Grabs weekly data for tweet.
@@ -88,10 +120,39 @@ def plotGraph(
     address family.
     """
 
+def create_message(deltaH: str, deltaW: str) -> str:
+  """Creates update message.
+  Uses the deltas to formualte a message to be tweeted. Message
+  depends on current values, six hour old values, and last weeks values
+  """
+  if deltaH == 1:
+      update = "This is 1 more prefix than 6 hours ago "
+  elif deltaH == -1:
+      update = "This is 1 less prefix than 6 hours ago "
+  elif deltaH < 0:
+      update = "This is " + str(-deltaH) + " fewer prefixes than 6 hours ago "
+  elif deltaH > 0:
+      update = "This is " + str(deltaH) + " more prefixes than 6 hours ago "
+  else:
+      update = "No change in the amount of prefixes from 6 hours ago "
+
+  if deltaW == 1:
+      update += "and 1 more than a week ago"
+  elif deltaW == -1:
+      update += "and 1 less than a week ago"
+  elif deltaW < 0:
+      update += "and " + str(-deltaW) + " fewer than a week ago"
+  elif deltaW > 0:
+      update += "and " + str(deltaW) + " more than a week ago"
+  else:
+      update += "and no change in the amount from a week ago"
+
+  return update
+
 def tweet(
     account: str,
+    message: str,
     image: bytes(),
-    message: str
     ):
     """Tweets to the world.
     Tweets the message, and image if it exists.
