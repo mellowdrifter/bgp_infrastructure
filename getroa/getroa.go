623d0e0034a0fbaf95129a76472b953d64700e68
Darren O'Connor <mellow.drifter@gmail.com>
2019-05-27 10:16:55 -0400

    Mostly finished getroa.

--- getroa/getroa.go
+++ getroa/getroa.go
@@ -4,13 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
 )
 
-const rpki = "https://rpki.cloudflare.com/rpki.json"
+const (
+	rpki    = "https://rpki.cloudflare.com/rpki.json"
+	logfile = "/var/log/rpki_update.log"
+	v4File  = "/etc/bird/v4.roa"
+	v6File  = "/etc/bird/v6.roa"
+)
 
 var example = []byte(`
 {
@@ -65,12 +72,43 @@ type roa struct {
 }
 
 func main() {
-	fmt.Printf("Downloading %s\n", path.Base(rpki))
-	fullroas := getROAs()
+	// set up log file
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed to open logfile: %v\n", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
 
+	// get and divide up the ROAs
+	log.Printf("Downloading %s\n", path.Base(rpki))
+	fullroas := getROAs()
 	v4, v6 := divideV4V6(fullroas)
 
-	fmt.Printf("There are %d IPv4 roas and %d IPv6 roas\n", len(v4), len(v6))
+	log.Printf("There are %d IPv4 roas and %d IPv6 roas\n", len(*v4), len(*v6))
+
+	// write updated roas to file and refresh bird
+	err = writeROAs(v4, v4File)
+	if err != nil {
+		log.Printf("unable to write IPv4 ROAs: %v", err)
+	}
+	log.Printf("file written")
+	cmd := exec.Command("birdc", "'configure'")
+	err = cmd.Run()
+	if err != nil {
+		log.Printf("unable to reconfigure bird: %v", err)
+	}
+	log.Printf("bird reconfigured")
+
+	err = writeROAs(v6, v6File)
+	if err != nil {
+		log.Printf("unable to write IPv6 ROAs: %v", err)
+	}
+	cmd = exec.Command("birdc6", "'configure'")
+	err = cmd.Run()
+	if err != nil {
+		log.Printf("unable to reconfigure bird6: %v", err)
+	}
 }
 
 func getROAs() *rpkiResponse {
@@ -89,10 +127,9 @@ func getROAs() *rpkiResponse {
 	json.Unmarshal(roaJSON, &r)
 
 	return r
-
 }
 
-func divideV4V6(r *rpkiResponse) ([]roa, []roa) {
+func divideV4V6(r *rpkiResponse) (*[]roa, *[]roa) {
 	var v4roa []roa
 	var v6roa []roa
 
@@ -104,6 +141,22 @@ func divideV4V6(r *rpkiResponse) ([]roa, []roa) {
 		}
 	}
 
-	return v4roa, v6roa
+	return &v4roa, &v6roa
+}
 
+func writeROAs(roas *[]roa, filename string) error {
+	tmpfile := "/tmp/roa"
+	file, err := os.Create(tmpfile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, roa := range *roas {
+		update := fmt.Sprintf("roa %s max %d as %s;\n", roa.Prefix, int(roa.Mask), roa.Asn[2:])
+		fmt.Fprintf(file, update)
+	}
+	os.Rename(tmpfile, filename)
+
+	return nil
 }
