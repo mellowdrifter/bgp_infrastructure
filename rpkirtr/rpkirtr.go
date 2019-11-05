21c563ce6495172aec5e79619fea7441242aa3ae
Darren O'Connor <mellow.drifter@gmail.com>
2019-11-04 21:58:20 -0500

    All state in struct. Contains list of clients

--- rpkirtr/rpkirtr.go
+++ rpkirtr/rpkirtr.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"os"
@@ -14,6 +13,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	_ "net/http/pprof"
@@ -32,15 +32,13 @@ const (
 	ripe    rir = 4
 
 	// refresh is the amount of seconds to wait until a new json is pulled.
-	refresh = 4 * time.Minute
-	//refresh = 10 * time.Second
+	// refresh = 4 * time.Minute
+	refresh = 60 * time.Second
 
 	// 8282 is the RFC port for RPKI-RTR
 	port = 8282
 	loc  = "localhost"
 )
-
-var cacheSerial uint32 = 0
 
 // enum used for RIRs
 type rir int
@@ -75,10 +73,18 @@ type roas struct {
 	Roas []jsonroa `json:"roas"`
 }
 
-// Server is our main thing
-type Server struct {
-	roas []roa
-	net  net.Conn
+// CacheServer is our main thing
+type CacheServer struct {
+	listener net.Listener
+	clients  []*client
+	roas     []roa
+	mutex    *sync.Mutex
+	serial   uint32
+}
+
+// Each client will have their own thing
+type client struct {
+	conn net.Conn
 }
 
 func main() {
@@ -104,42 +110,121 @@ func main() {
 	log.Printf("Downloading %s\n", roaFile)
 
 	// This is our server itself
-	var thing Server
+	var thing CacheServer
+	thing.mutex = &sync.Mutex{}
+	thing.listen()
 
-	// Keep ROAs updated
+	// ROAs should be updated all the time
 	go thing.readROAs(roaFile)
-	time.Sleep(time.Second * 5)
 
-	// Actually do something
-	// add := fmt.Sprintf("%s:%d", loc, port)
-	l, err := net.ListenTCP("tcp", &net.TCPAddr{Port: port})
-	if err != nil {
-		fmt.Printf("Unable to start server: %w", err)
-		os.Exit(1)
-	}
-	defer l.Close()
-	for {
-		conn, err := l.AcceptTCP()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		//thing.handleQuery()
-		go startSession(conn, thing.roas)
-	}
+	// I'm listening!
+	thing.start()
 
 }
 
-// readROAs will update the server struct with the current list of ROAs
-func (s *Server) readROAs(f string) {
+// Start listening
+func (s *CacheServer) listen() {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("Unable to start server: %v", err)
+	}
+	s.listener = l
+	fmt.Printf("Listening on port %d\n", port)
+
+}
+
+func (s *CacheServer) close() {
+	s.listener.Close()
+}
+
+func (s *CacheServer) start() {
 	for {
-		cacheSerial++
+		conn, err := s.listener.Accept()
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		} else {
+			client := s.accept(conn)
+			go s.serve(client)
+		}
+	}
+}
+
+func (s *CacheServer) serve(client *client) {
+	defer s.mutex.Unlock()
+	fmt.Printf("Serving %s\n", client.conn.RemoteAddr().String())
+	session := rand.Intn(100)
+
+	cpdu := cacheResponsePDU{
+		sessionID: uint16(session),
+	}
+	cpdu.serialize(client.conn)
+
+	s.mutex.Lock()
+
+	for _, roa := range s.roas {
+		IPAddress := net.ParseIP(roa.Prefix)
+		// TODO put ipv4/ipv6 signal in when creating the ROAs
+		switch strings.Contains(roa.Prefix, ":") {
+		case true:
+			ppdu := ipv6PrefixPDU{
+				flags:  uint8(1),
+				min:    uint8(roa.MinMask),
+				max:    uint8(roa.MaxMask),
+				prefix: IPAddress.To16(),
+				asn:    uint32(roa.ASN),
+			}
+			ppdu.serialize(client.conn)
+		case false:
+			ppdu := ipv4PrefixPDU{
+				flags:  uint8(1),
+				min:    uint8(roa.MinMask),
+				max:    uint8(roa.MaxMask),
+				prefix: IPAddress.To4(),
+				asn:    uint32(roa.ASN),
+			}
+			ppdu.serialize(client.conn)
+		}
+	}
+	fmt.Printf("Finished sending all prefixes")
+	epdu := endOfDataPDU{
+		sessionID: uint16(session),
+		//serial:    cacheSerial,
+		refresh: uint32(900),
+		retry:   uint32(30),
+		expire:  uint32(171999),
+	}
+	epdu.serialize(client.conn)
+
+}
+
+func (s *CacheServer) accept(conn net.Conn) *client {
+	fmt.Printf("Connection from %v, total clients: %d\n",
+		conn.RemoteAddr().String(), len(s.clients)+1)
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	client := &client{
+		conn: conn,
+	}
+
+	s.clients = append(s.clients, client)
+
+	return client
+}
+
+// readROAs will update the server struct with the current list of ROAs
+func (s *CacheServer) readROAs(f string) {
+	for {
+		s.mutex.Lock()
+		s.serial++
 		roas, err := readROAs(f)
 		if err != nil {
 			log.Fatal(err)
 		}
 		s.roas = roas
-		fmt.Println("roas updated")
+		fmt.Printf("roas updated, serial is now %d\n", s.serial)
+		s.mutex.Unlock()
 		time.Sleep(refresh)
 	}
 
@@ -205,70 +290,4 @@ func asnToInt(a string) int {
 	}
 
 	return n
-}
-
-func (s *Server) handleQuery() {
-	fmt.Printf("Incoming from %s\n", s.net.RemoteAddr().String())
-	defer s.net.Close()
-
-	buf := new(bytes.Buffer)
-	readBuf := make([]byte, 1024)
-
-	for {
-		dataLen, err := s.net.Read(readBuf)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Connection closed by client!")
-				break
-			}
-		}
-		buf.Write(readBuf[:dataLen])
-
-		fmt.Printf("*** %v\n", buf)
-
-	}
-}
-
-func startSession(conn *net.TCPConn, roas []roa) {
-	fmt.Printf("Incoming from %s\n", conn.RemoteAddr().String())
-
-	cpdu := cacheResponsePDU{
-		sessionID: uint16(123),
-	}
-	cpdu.serialize(conn)
-
-	for _, roa := range roas {
-		IPAddress := net.ParseIP(roa.Prefix)
-		// TODO put ipv4/ipv6 signal in when creating the ROAs
-		switch strings.Contains(roa.Prefix, ":") {
-		case true:
-			ppdu := ipv6PrefixPDU{
-				flags:  uint8(1),
-				min:    uint8(roa.MinMask),
-				max:    uint8(roa.MaxMask),
-				prefix: IPAddress.To16(),
-				asn:    uint32(roa.ASN),
-			}
-			ppdu.serialize(conn)
-		case false:
-			ppdu := ipv4PrefixPDU{
-				flags:  uint8(1),
-				min:    uint8(roa.MinMask),
-				max:    uint8(roa.MaxMask),
-				prefix: IPAddress.To4(),
-				asn:    uint32(roa.ASN),
-			}
-			ppdu.serialize(conn)
-		}
-	}
-	fmt.Printf("Finished sending all IPv4 prefixes")
-	epdu := endOfDataPDU{
-		sessionID: uint16(123),
-		serial:    cacheSerial,
-		refresh:   uint32(900),
-		retry:     uint32(30),
-		expire:    uint32(171999),
-	}
-	epdu.serialize(conn)
-
 }
