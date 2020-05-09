239e32f494c3f8dfd5be7100a155f37377b1261c
Darren O'Connor <mellow.drifter@gmail.com>
2020-05-09 13:25:33 -0400

    clean up faker

--- mocks/glass/glass.pb.go
+++ mocks/glass/glass.pb.go
@@ -21,14 +21,15 @@ func (l *lookingGlassClient) Origin(ctx context.Context, in *gpb.OriginRequest, 
 	var asn uint32
 	var exists bool
 
-	ip := in.IpAddress.GetAddress()
-
-	switch ip {
+	switch in.IpAddress.GetAddress() {
 	case "108.31.0.1":
 		asn = 701
 		exists = true
 	case "8.8.8.8":
 		asn = 15169
+		exists = true
+	case "1.1.1.1":
+		asn = 13335
 		exists = true
 	}
 	return &gpb.OriginResponse{
@@ -44,12 +45,53 @@ func (l *lookingGlassClient) Aspath(ctx context.Context, in *gpb.AspathRequest, 
 
 // route will return the full ip route output.
 func (l *lookingGlassClient) Route(ctx context.Context, in *gpb.RouteRequest, opts ...grpc.CallOption) (*gpb.RouteResponse, error) {
-	return &gpb.RouteResponse{}, nil
+	var route *gpb.IpAddress
+	var exists bool
+
+	switch in.IpAddress.GetAddress() {
+	case "108.31.0.1":
+		route = &gpb.IpAddress{
+			Address: "108.31.0.0",
+			Mask:    24,
+		}
+		exists = true
+	case "8.8.8.8":
+		route = &gpb.IpAddress{
+			Address: "8.8.8.0",
+			Mask:    24,
+		}
+		exists = true
+	}
+	return &gpb.RouteResponse{
+		IpAddress: route,
+		Exists:    exists,
+	}, nil
 }
 
 // asname will return the AS name.
 func (l *lookingGlassClient) Asname(ctx context.Context, in *gpb.AsnameRequest, opts ...grpc.CallOption) (*gpb.AsnameResponse, error) {
-	return &gpb.AsnameResponse{}, nil
+	var asname, locale string
+	var exists bool
+
+	switch in.GetAsNumber() {
+	case 701:
+		asname = "UUNET"
+		locale = "US"
+		exists = true
+	case 15169:
+		asname = "GOOGLE"
+		locale = "US"
+		exists = true
+	case 11111:
+		asname = "THOMSON-HEALTHCARE"
+		locale = "EU"
+		exists = true
+	}
+	return &gpb.AsnameResponse{
+		AsName: asname,
+		Locale: locale,
+		Exists: exists,
+	}, nil
 }
 
 // roa will return the roa status.
